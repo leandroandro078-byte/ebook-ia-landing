50e5b46a892b8d146faa7027d8a8349358512672
Leandro Artur <leandroandro078@gmail.com>
2026-06-13 23:16:17 +0100

    Adiciona contador de escassez

--- js/main.js
+++ js/main.js
@@ -449,3 +449,44 @@ document.querySelectorAll('.faq_question').forEach(button => {
     });
 
 });
+// CONTADOR
+
+const targetDate = new Date();
+targetDate.setHours(targetDate.getHours() + 6);
+
+function updateCountdown() {
+
+    const now = new Date();
+
+    const difference = targetDate - now;
+
+    const hours =
+        Math.floor(difference / (1000 * 60 * 60));
+
+    const minutes =
+        Math.floor(
+            (difference % (1000 * 60 * 60))
+            /
+            (1000 * 60)
+        );
+
+    const seconds =
+        Math.floor(
+            (difference % (1000 * 60))
+            /
+            1000
+        );
+
+    document.getElementById("hours").textContent =
+        String(hours).padStart(2, "0");
+
+    document.getElementById("minutes").textContent =
+        String(minutes).padStart(2, "0");
+
+    document.getElementById("seconds").textContent =
+        String(seconds).padStart(2, "0");
+}
+
+setInterval(updateCountdown, 1000);
+
+updateCountdown();
