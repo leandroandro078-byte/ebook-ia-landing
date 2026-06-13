4dcba847976d09a3bfecb906f5332bee1008d367
Leandro Artur <leandroandro078@gmail.com>
2026-06-13 16:08:42 +0100

    debug cloudinary

--- checkout/checkout.js
+++ checkout/checkout.js
@@ -1,6 +1,78 @@
-function enviarComprovativo() {
+async function enviarComprovativo() {
 
-    window.location.href =
-    "../obrigado/obrigado.html";
+    const file =
+        document.getElementById("comprovativo").files[0];
 
+    if (!file) {
+        alert("Selecione um comprovativo.");
+        return;
+    }
+
+    const formData = new FormData();
+
+    formData.append("file", file);
+
+    formData.append(
+        "upload_preset",
+        "ebook_comprovativos"
+    );
+
+    try {
+
+        const response =
+            await fetch(
+                "https://api.cloudinary.com/v1_1/drbnysxc2/auto/upload",
+                {
+                    method: "POST",
+                    body: formData
+                }
+            );
+
+        const data = await response.json();
+
+        console.log("Resposta Cloudinary:", data);
+
+        const comprovativoUrl = data.secure_url;
+
+        const emailData = new FormData();
+
+        emailData.append(
+            "Nome",
+            document.getElementById("nome").value
+        );
+
+        emailData.append(
+            "Email",
+            document.getElementById("email").value
+        );
+
+        emailData.append(
+            "WhatsApp",
+            document.getElementById("whatsapp").value
+        );
+
+        emailData.append(
+            "Comprovativo",
+            comprovativoUrl
+        );
+
+        await fetch(
+            "https://formsubmit.co/ajax/leandroandro078@gmail.com",
+            {
+                method: "POST",
+                body: emailData
+            }
+        );
+
+        window.location.href =
+            "../obrigado/obrigado.html";
+
+    } catch (error) {
+
+        alert(
+            "Erro ao enviar comprovativo."
+        );
+
+        console.error(error);
+    }
 }
